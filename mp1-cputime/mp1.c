e50a5543cee1d51f4b2a6a1e5ad42d82d6c8891b
Jun Luo <junluo2@illinois.edu>
2020-02-09 13:47:17 -0600

    Create and remove proc entries
    [MP 1]

--- mp1-cputime/mp1.c
+++ mp1-cputime/mp1.c
@@ -2,13 +2,35 @@
 
 #include <linux/module.h>
 #include <linux/kernel.h>
+#include <linux/proc_fs.h>
 #include "mp1_given.h"
 
 MODULE_LICENSE("GPL");
-MODULE_AUTHOR("Group_ID");
+MODULE_AUTHOR("LUOJL");
 MODULE_DESCRIPTION("CS-423 MP1");
 
 #define DEBUG 1
+#define FILENAME "status"
+#define DIRECTORY "mp1"
+
+static struct proc_dir_entry *proc_dir;
+static struct proc_dir_entry *proc_entry;
+
+static ssize_t mp1_read (struct file *file, char __user *buffer, size_t count, loff_t *data) {
+   printk(KERN_ALERT "mp1_read\n");
+   return 0;
+}
+
+static ssize_t mp1_write (struct file *file, const char __user *buffer, size_t count, loff_t *data) {
+   printk(KERN_ALERT "mp1_write\n");
+   return 0;
+}
+
+static const struct file_operations mp1_file = {
+   .owner = THIS_MODULE,
+   .read  = mp1_read,
+   .write = mp1_write,
+};
 
 // mp1_init - Called when module is loaded
 int __init mp1_init(void)
@@ -17,7 +39,8 @@ int __init mp1_init(void)
    printk(KERN_ALERT "MP1 MODULE LOADING\n");
    #endif
    // Insert your code here ...
-   
+   proc_dir = proc_mkdir(DIRECTORY, NULL);
+   proc_entry = proc_create(FILENAME, 0666, proc_dir, & mp1_file);
    
    
    printk(KERN_ALERT "MP1 MODULE LOADED\n");
@@ -31,8 +54,8 @@ void __exit mp1_exit(void)
    printk(KERN_ALERT "MP1 MODULE UNLOADING\n");
    #endif
    // Insert your code here ...
-   
-   
+   proc_remove(proc_entry);
+   proc_remove(proc_dir);
 
    printk(KERN_ALERT "MP1 MODULE UNLOADED\n");
 }
