665ab562bcaaf134898d7b7b08b04b5e69a68f60
Jun Luo <junluo2@illinois.edu>
2020-02-10 15:06:20 -0600

    Print "Hey there" in read callback
    [MP 1]

--- mp1-cputime/mp1.c
+++ mp1-cputime/mp1.c
@@ -1,5 +1,6 @@
 #define LINUX
 
+#include <linux/uaccess.h>
 #include <linux/module.h>
 #include <linux/kernel.h>
 #include <linux/proc_fs.h>
@@ -12,52 +13,65 @@ MODULE_DESCRIPTION("CS-423 MP1");
 #define DEBUG 1
 #define FILENAME "status"
 #define DIRECTORY "mp1"
+#define BUFSIZE 512
 
 static struct proc_dir_entry *proc_dir;
 static struct proc_dir_entry *proc_entry;
 
 static ssize_t mp1_read (struct file *file, char __user *buffer, size_t count, loff_t *data) {
-   printk(KERN_ALERT "mp1_read\n");
-   return 0;
+    char buf[BUFSIZE];
+    int len = 0;
+
+    printk(KERN_ALERT "mp1_read\n");
+    if (*data > 0 || count < BUFSIZE) {
+        return 0;
+    }
+    len += sprintf(buf, "Hey there\n");
+
+    if (copy_to_user(buffer, buf, len)) {
+       return -EFAULT;
+    }
+    *data = len;
+    return len;
 }
 
 static ssize_t mp1_write (struct file *file, const char __user *buffer, size_t count, loff_t *data) {
-   printk(KERN_ALERT "mp1_write\n");
-   return 0;
+    printk(KERN_ALERT "mp1_write\n");
+    return 0;
 }
 
 static const struct file_operations mp1_file = {
-   .owner = THIS_MODULE,
-   .read  = mp1_read,
-   .write = mp1_write,
+    .owner = THIS_MODULE,
+    .read  = mp1_read,
+    .write = mp1_write,
 };
 
 // mp1_init - Called when module is loaded
 int __init mp1_init(void)
 {
-   #ifdef DEBUG
-   printk(KERN_ALERT "MP1 MODULE LOADING\n");
-   #endif
-   // Insert your code here ...
-   proc_dir = proc_mkdir(DIRECTORY, NULL);
-   proc_entry = proc_create(FILENAME, 0666, proc_dir, & mp1_file);
-   
-   
-   printk(KERN_ALERT "MP1 MODULE LOADED\n");
-   return 0;   
+    #ifdef DEBUG
+    printk(KERN_ALERT "MP1 MODULE LOADING\n");
+    #endif
+    // Insert your code here ...
+    proc_dir = proc_mkdir(DIRECTORY, NULL);
+    proc_entry = proc_create(FILENAME, 0666, proc_dir, & mp1_file);
+
+
+    printk(KERN_ALERT "MP1 MODULE LOADED\n");
+    return 0;
 }
 
 // mp1_exit - Called when module is unloaded
 void __exit mp1_exit(void)
 {
-   #ifdef DEBUG
-   printk(KERN_ALERT "MP1 MODULE UNLOADING\n");
-   #endif
-   // Insert your code here ...
-   proc_remove(proc_entry);
-   proc_remove(proc_dir);
+    #ifdef DEBUG
+    printk(KERN_ALERT "MP1 MODULE UNLOADING\n");
+    #endif
+    // Insert your code here ...
+    proc_remove(proc_entry);
+    proc_remove(proc_dir);
 
-   printk(KERN_ALERT "MP1 MODULE UNLOADED\n");
+    printk(KERN_ALERT "MP1 MODULE UNLOADED\n");
 }
 
 // Register init and exit funtions
