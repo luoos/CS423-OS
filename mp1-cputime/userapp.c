d89874a2c1ebea5b2c693f54bf8bea64eb376a60
Jun Luo <junluo2@illinois.edu>
2020-02-11 14:19:59 -0600

    Implement a fibonacci func
    [MP 1]

--- mp1-cputime/userapp.c
+++ mp1-cputime/userapp.c
@@ -1,6 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include "userapp.h"
+
+int fib(int n) {
+	if (n < 2) {
+		return n;
+	}
+	return fib(n-1) + fib(n-2);
+}
 
 int main(int argc, char* argv[])
 {
+	int n, r;
+
+	if (argc != 2) {
+		printf("Usage: ./userapp <n>\n\n");
+		printf("Example:\n./userapp 20");
+	}
+
+	n = atoi(argv[1]);
+
+	r = fib(n);
+	printf("fib(%d)=%d\n", n, r);
 	return 0;
 }
