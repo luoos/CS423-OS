8bd23fab574279182d90b4e3b2f13261edd785a9
Jun Luo <junluo2@illinois.edu>
2020-03-03 13:33:54 -0600

    Exit when fail to parse command in mp1 userapp

--- mp1-cputime/userapp.c
+++ mp1-cputime/userapp.c
@@ -19,7 +19,8 @@ int main(int argc, char* argv[])
 
 	if (argc != 2) {
 		printf("Usage: ./userapp <n>\n\n");
-		printf("Example:\n./userapp 20");
+		printf("Example:\n./userapp 20\n");
+		return 1;
 	}
 
 	pid = getpid();
