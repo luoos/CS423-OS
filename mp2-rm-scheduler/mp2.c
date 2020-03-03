af2d903c7ca212b8e190bf97d0551c88a0a980a5
Jun Luo <junluo2@illinois.edu>
2020-03-03 17:03:58 -0600

    Add func to add and remove a task

--- mp2-rm-scheduler/mp2.c
+++ mp2-rm-scheduler/mp2.c
@@ -1,7 +1,12 @@
+#include <linux/types.h>
 #include <linux/uaccess.h>
 #include <linux/module.h>
 #include <linux/kernel.h>
 #include <linux/proc_fs.h>
+#include <linux/sched.h>
+#include <linux/list.h>
+#include <linux/mutex.h>
+#include <linux/slab.h>
 
 #include "mp2_given.h"
 
@@ -13,22 +18,71 @@ MODULE_DESCRIPTION("CS-423 MP2");
 #define DIRECTORY "mp2"
 #define READ_BUFSIZE  512
 #define WRITE_BUFSIZE 512
+#define STATE_SLEEPING 0
+#define STATE_READY 1
+#define STATE_RUNNING 2
 
 static struct proc_dir_entry *proc_dir;
 static struct proc_dir_entry *proc_entry;
 
 static char write_buffer[WRITE_BUFSIZE];
 
-void action_register(int pid, int period, int computation) {
-    printk(KERN_ALERT "registration, pid: %d, period: %d, computation: %d", pid, period, computation);
+static DEFINE_MUTEX(RMS_tasks_lock);
+static LIST_HEAD(tasks_list);
+
+// RMS: Rate-Monotonic CPU Scheduler
+typedef struct RMS_task_struct {
+    struct task_struct* linux_task;
+    struct timer_list wakeup_timer;
+    struct list_head lis;
+    int state;
+    uint period;
+    uint computation;
+} RMS_task;
+
+void __add_task(RMS_task *task) {
+    mutex_lock(&RMS_tasks_lock);
+    list_add(&(task->lis), &tasks_list);
+    mutex_unlock(&RMS_tasks_lock);
+    printk(KERN_ALERT "added task, pid: %d", task->linux_task->pid);
 }
 
-void action_yield(int pid) {
+void __del_task(pid_t pid) {
+    RMS_task *task;
+    struct list_head *ptr;
+    struct list_head *tmp;
+
+    mutex_lock(&RMS_tasks_lock);
+    list_for_each_safe(ptr, tmp, &tasks_list) {
+        task = list_entry(ptr, RMS_task, lis);
+        if (task->linux_task->pid == pid) {
+            list_del(ptr);
+            kfree(task);
+            printk(KERN_ALERT "deleted task, pid: %d", pid);
+            break;
+        }
+    }
+    mutex_unlock(&RMS_tasks_lock);
+}
+
+void action_register(uint pid, uint period, uint computation) {
+    RMS_task *t = (RMS_task *) kmalloc(sizeof(RMS_task), GFP_KERNEL);
+    struct task_struct *ts = find_task_by_pid(pid);
+    printk(KERN_ALERT "registration, pid: %d, period: %d, computation: %d", pid, period, computation);
+    t->linux_task = ts;
+    t->state = STATE_SLEEPING;
+    t->period = period;
+    t->computation = computation;
+    __add_task(t);
+}
+
+void action_yield(uint pid) {
     printk(KERN_ALERT "yield, pid: %d", pid);
 }
 
-void action_deregister(int pid) {
+void action_deregister(uint pid) {
     printk(KERN_ALERT "deregistration: pid: %d", pid);
+    __del_task(pid);
 }
 
 static ssize_t file_read (struct file *file, char __user *buffer, size_t count, loff_t *data) {
@@ -55,7 +109,8 @@ static ssize_t file_read (struct file *file, char __user *buffer, size_t count, 
  */
 static ssize_t file_write (struct file *file, const char __user *buffer, size_t count, loff_t *data) {
     int buffer_size = count;
-    int pid, period, computation, n;
+    uint pid, period, computation;
+    int n;
     char action;
     if (count > WRITE_BUFSIZE) {
         buffer_size = WRITE_BUFSIZE;
