2a13f4281b7683f92a9cf35697cb80ec5754dd2a
Jun Luo <junluo2@illinois.edu>
2020-03-05 14:59:59 -0600

    Add yield and factorial in userapp

--- mp2-rm-scheduler/userapp.c
+++ mp2-rm-scheduler/userapp.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <sys/time.h>
 
 #define PROC_FILE "/proc/mp2/status"
 #define true 1
@@ -48,6 +49,12 @@ void sched_register(uint pid, uint period, uint computation) {
     printf("[Registered] pid: %d, period: %d, computation: %d\n", pid, period, computation);
 }
 
+void sched_yield(uint pid) {
+    char str[32];
+    sprintf(str, "Y,%d", pid);
+    write_to_file(str);
+}
+
 void sched_degister(uint pid) {
     char s[64];
     sprintf(s, "D,%d", pid);
@@ -55,18 +62,42 @@ void sched_degister(uint pid) {
     printf("[Deregistered] pid: %d\n", pid);
 }
 
+unsigned long factorial(uint n) {
+    unsigned long ans = 1;
+    for (int i = 1; i <= n; i++) {
+        ans *= i;
+    }
+    return ans;
+}
+
+void print_time_gap(struct timeval *start, struct timeval *end) {
+    unsigned long start_ms = start->tv_sec * 1000 + (start->tv_usec / 1000);
+    unsigned long end_ms = end->tv_sec * 1000 + (end->tv_usec / 1000);
+    printf("time elapse: %lu ms\n", end_ms - start_ms);
+}
+
 int main(int argc, char* argv[]) {
-    uint pid, period, computation;
+    uint pid, n, period, computation;
+    struct timeval start, end;
 
     pid = getpid();
 
-    period = atoi(argv[1]);
-    computation = atoi(argv[2]);
+    n = atoi(argv[1]);
+    period = atoi(argv[2]);
+    computation = atoi(argv[3]);
 
     sched_register(pid, period, computation);
     if (!check_exist(pid)) {
         printf("pid doesn't exist, exit...\n");
         exit(1);
+    }
+
+
+    for (int i = 0; i < 10; i++) {
+        gettimeofday(&start, NULL);
+        factorial(n);
+        gettimeofday(&end, NULL);
+        print_time_gap(&start, &end);
     }
 
     sched_degister(pid);
